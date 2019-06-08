bfac6fcd3facc7d963e7a32e0c809fac43850df0
Zevi Liao <jointjm@gmail.com>
2019-06-08 23:16:06 +0800

    function

--- index.ts
+++ index.ts
@@ -1,17 +1,8 @@
-const isOpen: boolean = false;
-const myName: string = 'zevi'
 
-const myAge: number = 23
-const list: number[] = [0, 1, 2]
-
-const me: [string, number, boolean] = ['zevi', 32, false]
-
-enum Job {
-    WebDev, WebDesigner, PM
+const sayHello = (word?: string): string => {
+    console.log(word || 'hello')
+    return word || 'hello'
 }
 
-const job: Job = Job.WebDev
-
-const phone: any = "Pixel"
-const tablet: any = 3
+sayHello()
 
