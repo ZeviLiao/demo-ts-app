120e85d97905b1ef9c8167fceda25c4a2072958c
Zevi Liao <jointjm@gmail.com>
2019-06-08 22:33:47 +0800

    add project

---
+++ README.md
@@ -0,0 +1,12 @@
+ref:  
+https://www.leveluptutorials.com/tutorials
+
+
+```
+yarn global add parcel-bundler
+parcel index.html
+```
+
+```
+yarn add parcel-plugin-typescript parcel-bundler
+```
---
+++ index.ts
@@ -0,0 +1,17 @@
+const isOpen: boolean = false;
+const myName: string = 'zevi'
+
+const myAge: number = 23
+const list: number[] = [0, 1, 2]
+
+const me: [string, number, boolean] = ['zevi', 32, false]
+
+enum Job {
+    WebDev, WebDesigner, PM
+}
+
+const job: Job = Job.WebDev
+
+const phone: any = "Pixel"
+const tablet: any = 3
+
